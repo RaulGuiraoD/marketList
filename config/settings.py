41f672a616ec47f7f549d5098c35855bac132128
Raul <r.guirao@aragonphotonics.com>
2026-04-10 08:24:10 +0200

    Añadido a allowed_hosto --> * para que no de fallos para las URLs

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ DEBUG = os.getenv('DEBUG') == 'True'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['swiftlist-1.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
