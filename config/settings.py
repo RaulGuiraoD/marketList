dfb6aa3b02fd4918f71d526cfe6b1d879194ac2a
Raul <r.guirao@aragonphotonics.com>
2026-05-25 10:12:41 +0200

    Creación de recuperación de contraseñas via email

--- config/settings.py
+++ config/settings.py
@@ -143,3 +143,12 @@ LOGOUT_REDIRECT_URL = 'login'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'raulguiraodiaz@gmail.com'
+# IMPORTANTE: Aquí NO va tu contraseña normal de Gmail. Lee el paso de abajo.
+EMAIL_HOST_PASSWORD = 'aliihlbhmachqldr' 
+DEFAULT_FROM_EMAIL = 'MarketList <raulguiraodiaz@gmail.com>'
--- shopping/urls.py
+++ shopping/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('lista/<int:lista_id>/', views.ver_lista, name='ver_lista'),
@@ -12,4 +13,26 @@ urlpatterns = [
     path('archivadas/', views.listas_archivadas, name='listas_archivadas'),
     path('lista/reabrir/<int:lista_id>/', views.reabrir_lista, name='reabrir_lista'),
     path('archivadas/eliminar-multiple/', views.eliminar_multiple_listas, name='eliminar_multiple_listas'),
+    
+    path('password-reset/', 
+         auth_views.PasswordResetView.as_view(
+             template_name='registration/password_reset_form.html',
+             html_email_template_name='registration/password_reset_email.html' # 🌟 NUEVA LÍNEA
+         ), 
+         name='password_reset'),
+    
+    # 2. Pantalla que confirma que el correo ha sido enviado
+    path('password-reset/done/', 
+         auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), 
+         name='password_reset_done'),
+    
+    # 3. Enlace con token seguro que le llega al usuario a su email
+    path('password-reset-confirm/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), 
+         name='password_reset_confirm'),
+    
+    # 4. Pantalla que confirma que la contraseña se cambio correctamente
+    path('password-reset-complete/', 
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), 
+         name='password_reset_complete'),
 ]
